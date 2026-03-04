52d854537da7cd284b1f06a3f3e682ebea5bb2c9
Thehabibgamerz <thehabibgamerz@gmail.com>
2026-03-04 18:26:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+let db = JSON.parse(fs.readFileSync("./database.json"));
+const saveDB = () =>
+  fs.writeFileSync("./database.json", JSON.stringify(db, null, 2));
+
+app.get("/api/routes", (req, res) => {
+  res.json(db.weeklyRoutes);
+});
+
+app.post("/api/routes", (req, res) => {
+  const { day, multiplier, routes } = req.body;
+  db.weeklyRoutes[day] = { multiplier, routes };
+  saveDB();
+  res.json({ success: true });
+});
+
+app.listen(PORT, () =>
+  console.log(`🌐 Dashboard running on ${PORT}`)
+);
